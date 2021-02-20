e95354bf7b6db4d1f77e33a90ccce311b5af35e2
paulopompeu <pompeutoledo@gmail.com>
2021-02-19 23:24:18 -0600

    adding compression

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const compression = require('compression')
 
 const app = express()
 app.use(cors())
+app.use(compression())
 
 app.use('/', require('./routes/postsRoutes'))
 
